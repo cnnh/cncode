83fb4869a3753a4a74c651fc7c1417a928db5d32
false07 <984110211@qq.com>
2020-07-05 16:56:00 +0800

    Helloworld 添加备注

--- day_01/src/cn/test/dayo1/demo01/HelloWorld.java
+++ day_01/src/cn/test/dayo1/demo01/HelloWorld.java
@@ -1,5 +1,24 @@
 package cn.test.dayo1.demo01;
 
+/**
+ * Created by beigui on 2016/1/26.
+ * 功能：控制台打印输出Hello World。
+ * 确认java配置，开始java之旅
+ * 说明：
+ *  Hello World.java -- java源文件
+ *      |
+ *      |   javac HelloWorld.java
+ *      |
+ *  Hello World.class -- idea里在target的对应包下，编译后的字节码文件
+ *      |
+ *      |   java HelloWorld
+ *      |
+ *  JVM -- java虚拟机，字节码文件在java虚拟机中执行
+ *      |
+ *      |
+ *  > Hello World!
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
