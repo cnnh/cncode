1c909809ab68f8625cff218e1bc18456aa811ea8
false07 <984110211@qq.com>
2020-07-05 16:15:03 +0800

    Helloworld

---
+++ day_01/src/cn/test/dayo1/demo01/BasicDataTypes.java
@@ -0,0 +1,23 @@
+package cn.test.dayo1.demo01;
+/**
+ * 功能：java基本 数据类型介绍
+ */
+
+public class BasicDataTypes {
+    public static void main(String[] args) {
+        byte bt = 127;
+        System.out.println(bt);
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("c = "+c);
+        boolean flag =true;
+        System.out.println("flag =" + flag);
+        flag = false;
+        System.out.println("flag =" + flag);
+        char c1='中';
+        System.out.println(c1);
+
+
+    }
+}
