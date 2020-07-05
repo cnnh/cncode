ebf8bb40330ba777c2aa9e085e52078b0334f559
false07 <984110211@qq.com>
2020-07-05 16:47:42 +0800

    BasicDataTypes备注

--- day_01/src/cn/test/dayo1/demo01/BasicDataTypes.java
+++ day_01/src/cn/test/dayo1/demo01/BasicDataTypes.java
@@ -1,8 +1,14 @@
 package cn.test.dayo1.demo01;
 /**
- * 功能：java基本 数据类型介绍
+ * Created by beigui on 2016/1/28.
+ * 功能：java基本数据类型介绍
+ * 基本数据类型：
+ *      |--整数类型 byte(1个字节) short(2个字节) int(4个字节) long(8个字节)
+ *      |--小数（浮点）类型  float double
+ *      |--布尔类型 boolean(true/false)
+ *      |--字符类型 char 两个字节
+ * 注意：String不是基本数据类型，而是类
  */
-
 public class BasicDataTypes {
     public static void main(String[] args) {
         byte bt = 127;
