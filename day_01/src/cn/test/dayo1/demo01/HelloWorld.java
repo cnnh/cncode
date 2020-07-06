f5e9284823e15bd504f33032b50087a9d9da28a2
false07 <984110211@qq.com>
2020-07-06 14:15:34 +0800

    好难啊 git

--- day_01/src/cn/test/dayo1/demo01/HelloWorld.java
+++ day_01/src/cn/test/dayo1/demo01/HelloWorld.java
@@ -22,5 +22,7 @@ package cn.test.dayo1.demo01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World！好难啊！！！！！！！！！！");
+
     }
 }
